484de43abda07eba5dfd15f0d9ea4933cd9c125f
Medusa <medusa@medusadev1.library.illinois.edu>
2015-01-30 15:39:56 -0600

    use short hostname

--- backup-to-cnfs.rb
+++ backup-to-cnfs.rb
@@ -2,7 +2,7 @@
 require 'pathname'
 require 'date'
 
-hostname = `hostname`
+hostname = `hostname -s`
 BACKUP_ROOT = "/mnt/cnfs/medusa_misc/backup/#{hostname}"
 GLACIER_ROOT = '/services/medusa/medusa-glacier'
 MEDUSA_SHARED_ROOT = '/services/medusa/medusa-rails3-capistrano/shared'
