00095b813e71a1ef4ab5a29d8052ccdbcaf23b74
Medusa service account <medusa@aws-medusa-prod.library.illinois.edu>
2019-04-12 10:36:46 -0500

    Remove some unused code. Make mount script use env variables more.

--- rclone-mount.sh
+++ rclone-mount.sh
@@ -5,6 +5,5 @@ source ./env.sh
 mkdir -p $RCLONE_MOUNTPOINT
 mkdir -p $RCLONE_PIDDIR
 
-rclone mount medusa-user:medusa-main $HOME/mnt/medusa-main-ro --read-only &
-#rclone mount medusa-user:medusa-main $HOME/mnt/medusa-main-ro --read-only --log-file $HOME/bin/rclone.log -vv &
+rclone mount $RCLONE_REMOTE:$RCLONE_BUCKET $RCLONE_MOUNTPOINT --read-only &
 echo $! > $RCLONE_PIDFILE
--- rclone-unmount.sh
+++ rclone-unmount.sh
@@ -2,11 +2,3 @@
 source ./env.sh
 
 fusermount -uz $RCLONE_MOUNTPOINT
-# if [[ -f $RCLONE_PIDFILE ]]; then
-#     pid=$(cat $RCLONE_PIDFILE)
-#     echo "Killing rclone mount. PID: $pid"
-#     kill $pid
-#     rm -f $RCLONE_PIDFILE
-# else
-#     echo "Rclone pidfile not found"
-# fi
