4eb5cb075b880b9d34aa14b470aa112d5ec81f65
Medusa service account <medusa@aws-medusa-prod.library.illinois.edu>
2019-04-12 10:25:58 -0500

    Simplify rclone operations.

--- rclone-mount.sh
+++ rclone-mount.sh
@@ -6,4 +6,5 @@ mkdir -p $RCLONE_MOUNTPOINT
 mkdir -p $RCLONE_PIDDIR
 
 rclone mount medusa-user:medusa-main $HOME/mnt/medusa-main-ro --read-only &
+#rclone mount medusa-user:medusa-main $HOME/mnt/medusa-main-ro --read-only --log-file $HOME/bin/rclone.log -vv &
 echo $! > $RCLONE_PIDFILE
