4b3ceb3268acb33869783a866f798b06ceecff5d
Medusa service account <medusa@aws-medusa-prod.library.illinois.edu>
2019-06-27 13:41:34 -0500

    Always try to unmount before remounting

--- rclone-mount.sh
+++ rclone-mount.sh
@@ -5,6 +5,7 @@ source ./env.sh
 mkdir -p $RCLONE_MOUNTPOINT
 mkdir -p $RCLONE_PIDDIR
 
-mountpoint -q $RCLONE_MOUNTPOINT && fusermount -uz $RCLONE_MOUNTPOINT
+#mountpoint -q $RCLONE_MOUNTPOINT && fusermount -uz $RCLONE_MOUNTPOINT
+fusermount -uz $RCLONE_MOUNTPOINT
 rclone mount $RCLONE_REMOTE:$RCLONE_BUCKET $RCLONE_MOUNTPOINT --read-only &
 echo $! > $RCLONE_PIDFILE
