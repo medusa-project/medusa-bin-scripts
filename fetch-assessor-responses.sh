bb72b04cf4cef9de3bfa3188e445bc5a4f76a85a
Colleen Fallaw <mfall3@illinois.edu>
2022-02-15 07:04:37 -0600

    add script to fetch assessor responses

---
+++ fetch-assessor-responses.sh
@@ -0,0 +1,9 @@
+#!/bin/bash --login
+#fetch responses from the Medusa Assessor Service
+
+source $HOME/bin/env.sh
+
+#fetch responses
+( cd $MEDUSA_HOME ; bundle exec rake assessor:fetch_messages ; bundle exec rake assessor:handle_fetched_messages )
+
+exit 0
